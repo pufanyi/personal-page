677c8b496417ec74b9abf5cd44078f95b6187d6a
pufanyi <pufanyi@sensetime.com>
2026-02-18 00:39:24 +0800

    refactor: update directive selector from autoAnimate to appAutoAnimate across components

--- src/app/directives/auto-animate.ts
+++ src/app/directives/auto-animate.ts
@@ -2,7 +2,7 @@ import { AfterViewInit, Directive, ElementRef, inject } from '@angular/core';
 import autoAnimate from '@formkit/auto-animate';
 
 @Directive({
-  selector: '[autoAnimate]',
+  selector: '[appAutoAnimate]',
 })
 export class AutoAnimateDirective implements AfterViewInit {
   private el = inject(ElementRef<HTMLElement>);
